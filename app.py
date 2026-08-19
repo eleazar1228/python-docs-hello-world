8a6c37b5b21b0d32666ae05787cd3759e684789b
eleazar1228 <65876459+eleazar1228@users.noreply.github.com>
2026-08-19 18:53:18 -0400

    Enhance hello function with staging message
    Updated the hello function to include a staging message.

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! This is Eleazar! I love Jesus, and my wife Sydney!!"
+    return "Hello, World! This is Eleazar! I love Jesus, and my wife Sydney!!
+    This is the staging stage by the way!"
