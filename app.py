202d10012ab7f3f0c94268b4169d885783b9bda6
eleazar1228 <65876459+eleazar1228@users.noreply.github.com>
2026-08-19 19:11:49 -0400

    Remove newline in hello() return statement

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! This is Eleazar! I love Jesus, and my wife Sydney!!
-    This is the staging stage by the way!"
+    return "Hello, World! This is Eleazar! I love Jesus, and my wife Sydney!!This is the staging stage by the way!"
